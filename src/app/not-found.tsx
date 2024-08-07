6606ddb361fbb68f8279eb62a4e57b8baeebb305
Roman Larionov <rommalarionov00@gmail.com>
2024-08-07 16:40:15 +0800

    add timers in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const NotFound = () => {
+  const [time, setTime] = useState(5);
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className="flex min-h[100%] flex-col items-center justify-between p-20 bg-white text">
-      <h1 className="text-3xl p-2">404 Page Not Found</h1>
+      <h1 className="text-3xl p-2 mb-4">404 Page Not Found</h1>
+      <Link
+        href="/"
+        className="font-bold text-2xl duration-500 hover:scale-125 "
+      >
+        Перейти на главную
+      </Link>
+      или автоматически через {time} сек
     </main>
   );
 };
